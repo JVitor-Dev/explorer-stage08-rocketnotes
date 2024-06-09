0c526ef2e55319b3e236c6fabd15c57b0cff53a3
João Vitor <jvitordev@outlook.com>
2024-06-09 18:30:15 -0300

    biblioteca cors e axios implantadas

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ const migrationsRun = require('./database/sqlite/migrations')
 const AppError = require('./utils/AppError')
 const uploadConfig = require('./configs/upload')
 
+const cors = require('cors') // conectando front com back end
 const express = require('express')
 const routes = require('./routes')
 
 migrationsRun()
 
 const app = express()
+app.use(cors()) //back atendendo o front
 app.use(express.json())
 
 app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
