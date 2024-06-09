e0dbbda39617f30668229b5a39d4239d4b3ede7e
João Vitor <jvitordev@outlook.com>
2024-06-09 17:23:59 -0300

    comentarios das funções diskstorage

--- src/providers/DiskStorage.js
+++ src/providers/DiskStorage.js
@@ -5,7 +5,7 @@ const uploadConfig = require('../configs/upload')
 class DiskStorage {
   async saveFile(file) {
     await fs.promises.rename(
-      //mudar o arquivo de lugar
+      //mudar o arquivo de lugar - 'rename' renomear ou mover arquivos
       path.resolve(uploadConfig.TMP_FOLDER, file),
       path.resolve(uploadConfig.UPLOAD_FOLDER, file)
     )
@@ -16,12 +16,12 @@ class DiskStorage {
   async deleteFile(file) {
     const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file) //pegar o endereço do arquivo existente
     try {
-      await fs.promises.stat(filePath)
+      await fs.promises.stat(filePath) //stat = retorna estado do arquivo, se ta aberto, existe ou não, etc
     } catch {
       return
     }
 
-    await fs.promises.unlink(filePath) //deletar o arquivo
+    await fs.promises.unlink(filePath) //deletar o arquivo - 'unlink' deleta arquivos
   }
 }
 
